e0f64616830c6ddbe7b47f81f3ba220ff4ab354c
Monika Jadhav <monikajadhav1211@gmail.com>
2025-11-04 18:29:04 +0530

    Add files via upload

---
+++ LogicalOperator.java
@@ -0,0 +1,23 @@
+package com.demo;
+
+public class LogicalOperator {
+	public static void main(String[] args) {
+		
+		//logical AND &&
+		System.out.println(true && true);
+		System.out.println(true && false);
+		System.out.println(false && true);
+		System.out.println(false && false);
+		
+		//logical OR ||
+		System.out.println(true || true);
+		System.out.println(true || false);
+		System.out.println(false || true);
+		System.out.println(false || false);
+		
+		//logical NOT !
+		System.out.println(!true);   
+		System.out.println(!false);
+	}
+
+}
