614d00c7b62dda99e7c9475348000eb9c2011303
Monika Jadhav <monikajadhav1211@gmail.com>
2025-11-05 18:11:01 +0530

    Add files via upload

---
+++ PostIncreDecreOperator.java
@@ -0,0 +1,18 @@
+package com.demo;
+
+public class PostIncreDecreOperator {
+	    public static void main(String[] args) {
+	        int a=10;
+	
+	        System.out.println(a--);
+	        System.out.println(a++);
+	        System.out.println(a++);
+	        System.out.println(a--);
+	        System.out.println(a++);
+	        System.out.println(a--);
+	        System.out.println(a--);
+	        
+	        System.out.println(a);
+	    }
+	}
+
