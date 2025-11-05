2f41ff2230d128c0ad7f91d64e2c3b8908e88ed5
Monika Jadhav <monikajadhav1211@gmail.com>
2025-11-05 18:24:11 +0530

    Add files via upload

---
+++ PreIncreDecreOperator.java
@@ -0,0 +1,19 @@
+package com.demo;
+
+public class PreIncreDecreOperator {
+	    public static void main(String[] args) {
+	        int a=3;
+	
+	        System.out.println(++a);
+	        System.out.println(++a);
+	        a++;
+	        a--;
+	        
+	        System.out.println(--a);
+	        System.out.println(--a);
+	        System.out.println(--a);
+	        
+	        System.out.println(a);
+	    }
+	}
+
