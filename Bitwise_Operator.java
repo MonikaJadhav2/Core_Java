e9361df055a0babe8317ee0431a9f3551340dbf1
Monika Jadhav <monikajadhav1211@gmail.com>
2025-11-07 18:19:27 +0530

    Add files via upload

---
+++ Bitwise_Operator.java
@@ -0,0 +1,20 @@
+package com.demo;
+
+public class Bitwise_Operator {
+	public static void main(String[] args) {
+		
+		System.out.println(4 & 23);
+		System.out.println(5 & 43);
+		
+		System.out.println(11 | 19);
+		System.out.println(14 | 34);
+		
+		System.out.println(12 & 15);
+		System.out.println(11 | 13 & 12);
+		System.out.println(17 & 7 & 49);
+		
+		System.out.println(11 ^ 12);
+		
+	}
+
+}
