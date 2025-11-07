2bb21aed32b01a19dc629e0a73446679c2d413d9
Monika Jadhav <monikajadhav1211@gmail.com>
2025-11-07 17:39:13 +0530

    Add files via upload

---
+++ Decimal_To_Binary.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+public class Decimal_To_Binary {
+	public static void main(String[] args) {
+		int decimal = 15;
+		System.out.println(Integer.toBinaryString(decimal));
+		
+		int decimal1 = 45;
+		System.out.println(Integer.toBinaryString(decimal1));
+		
+		
+	}
+
+}
