1fd482090efbedf7ade59ba430d9b147bc73b042
Monika Jadhav <monikajadhav1211@gmail.com>
2025-11-10 17:40:45 +0530

    Add files via upload

---
+++ Bitwise_XOR.java
@@ -0,0 +1,18 @@
+package com.demo;
+
+public class Bitwise_XOR {
+	public static void main(String[] args) {
+		
+		
+		        int a = 5;   // binary: 0101
+		        int b = 3;   // binary: 0011
+		        int c = 7;   //binary: 0111
+
+		        int result = a ^ b;  // XOR operation
+		        int result1 = a ^ c;
+
+		        System.out.println("a ^ b = " + result);
+		        System.out.println("a ^ c = " + result1);
+		    }
+
+}
