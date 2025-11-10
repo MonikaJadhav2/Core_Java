191f31628f8f030493f22d0557b60a768aec5390
Monika Jadhav <monikajadhav1211@gmail.com>
2025-11-10 17:48:40 +0530

    Add files via upload

---
+++ Bitwise_NOT.java
@@ -0,0 +1,18 @@
+package com.demo;
+
+public class Bitwise_NOT {
+	public static void main(String[] args) {
+		
+		
+		        int a = 5;   // binary: 0101
+		        int b = 3;   // binary: 0011
+		        int c = 7;   //binary: 0111
+
+		       
+
+		        System.out.println("~ a= " + ~a );
+		        System.out.println("~ b = " + ~b);
+		        System.out.println("~ c = " + ~c);
+		    }
+
+}
