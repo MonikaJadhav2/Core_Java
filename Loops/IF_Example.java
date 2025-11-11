024eca6c9f72e4637a1339324f199439d23a85dd
Monika Jadhav <monikajadhav1211@gmail.com>
2025-11-11 18:26:43 +0530

    Add files via upload

---
+++ Loops/IF_Example.java
@@ -0,0 +1,23 @@
+package com.loops;
+
+public class IF_Example {
+	public static void main(String[] args) {
+		int number = 10;
+
+        // Check if number is positive
+        if (number > 0) {
+            System.out.println("The number is positive.");
+        }
+
+        // Check if number is negative
+        if (number < 0) {
+            System.out.println("The number is negative.");
+        }
+
+        // Check if number is zero
+        if (number == 0) {
+            System.out.println("The number is zero.");
+        }
+	}
+
+}
