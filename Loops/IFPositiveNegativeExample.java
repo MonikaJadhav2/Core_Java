7dff55cecd30e5d0a1d4ed89d95443978eea987d
Monika Jadhav <monikajadhav1211@gmail.com>
2025-11-11 18:39:46 +0530

    Add files via upload

---
+++ Loops/IFPositiveNegativeExample.java
@@ -0,0 +1,18 @@
+package com.loops;
+
+public class IFPositiveNegativeExample {
+	
+	    public static void main(String[] args) {
+	        
+	        int number = -5; // You can change this value to test different numbers
+
+	        if (number > 0) {
+	            System.out.println(number + " is a positive number.");
+	        } else if (number < 0) {
+	            System.out.println(number + " is a negative number.");
+	        } else {
+	            System.out.println("The number is zero.");
+	        }
+	    
+	    }
+}
