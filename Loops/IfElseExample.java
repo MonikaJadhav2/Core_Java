3fbb09d07757eae411a243cd08a981ce2a69cc47
Monika Jadhav <monikajadhav1211@gmail.com>
2025-11-11 18:29:21 +0530

    Add files via upload

---
+++ Loops/IfElseExample.java
@@ -0,0 +1,15 @@
+package com.loops;
+
+public class IfElseExample {
+	public static void main(String[] args) {
+		 int number = 15;
+
+	        // Check if the number is even or odd
+	        if (number % 2 == 0) {
+	            System.out.println(number + " is even");
+	        } else {
+	            System.out.println(number + " is odd");
+	        }
+	}
+
+}
