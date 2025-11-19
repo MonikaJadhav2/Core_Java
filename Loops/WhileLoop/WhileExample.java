2b8498d3291685d63b2964dec211edf53f263ab8
Monika Jadhav <monikajadhav1211@gmail.com>
2025-11-19 16:12:00 +0530

    Add files via upload

---
+++ Loops/WhileLoop/WhileExample.java
@@ -0,0 +1,12 @@
+
+public class WhileExample {
+	public static void main(String[] args) {
+		int i = 1; // initialization
+
+		while (i <= 5) { // condition
+			System.out.println(i);
+			i++; // updation
+		}
+
+	}
+}
