06f74ef1c234132068924fdcaf5081d1957b0e08
Monika Jadhav <monikajadhav1211@gmail.com>
2025-11-19 16:12:43 +0530

    Add files via upload

---
+++ Loops/DoWhileLoop/DoWhileExample.java
@@ -0,0 +1,12 @@
+
+public class DoWhileExample {
+	public static void main(String[] args) {
+		int i = 1; // initialization
+
+		do {
+			System.out.println(i);
+			i++;           // updation
+		} while (i <= 5); // condition
+
+	}
+}
