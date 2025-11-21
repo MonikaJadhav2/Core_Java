ac2fa8cdc70ccfc19afefdcceaa64b4e137a745a
Monika Jadhav <monikajadhav1211@gmail.com>
2025-11-21 17:26:34 +0530

    Add files via upload

---
+++ Loops/BreaknContinue/BreakandContinue.java
@@ -0,0 +1,27 @@
+
+public class BreakandContinue {
+
+	public static void main(String[] args) {
+
+		for (int i = 1; i <= 10; i++) {
+			int product = 9 * i;
+			if (product > 50) {
+				break;
+			}
+			System.out.println(product);
+		}
+
+		/*
+		 * for (char ch = 'A'; ch <= 'Z'; ch++) {
+		 * 
+		 * if (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+		 * continue; }
+		 * 
+		 * System.out.println(ch);
+		 * 
+		 * }
+		 */
+
+	}
+
+}
