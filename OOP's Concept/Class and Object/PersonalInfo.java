b821efc203cecb26b5b33771b467a5b7004d637e
Monika Jadhav <monikajadhav1211@gmail.com>
2025-11-25 19:21:05 +0530

    Personal Information printing

---
+++ OOP's Concept/Class and Object/PersonalInfo.java
@@ -0,0 +1,155 @@
+package com.oops;
+
+public class PersonalInfo {
+
+	String name;
+	int age;
+	String city;
+	long mobileNumber;
+	String email;
+
+	public static void main(String[] args) {
+
+		PersonalInfo p1 = new PersonalInfo();
+		p1.name = "Monika";
+		p1.age = 23;
+		p1.city = "Pune";
+		p1.mobileNumber = 9876543210L;
+		p1.email = "monika@example.com";
+
+		PersonalInfo p2 = new PersonalInfo();
+		p2.name = "komal";
+		p2.age = 25;
+		p2.city = "Mumbai";
+		p2.mobileNumber = 9876501234L;
+		p2.email = "komal@example.com";
+
+		PersonalInfo p3 = new PersonalInfo();
+		p3.name = "Snehal";
+		p3.age = 22;
+		p3.city = "Nashik";
+		p3.mobileNumber = 9871234500L;
+		p3.email = "snehal@example.com";
+
+		PersonalInfo p4 = new PersonalInfo();
+		p4.name = "Shreya";
+		p4.age = 24;
+		p4.city = "Nagpur";
+		p4.mobileNumber = 9871112223L;
+		p4.email = "shreya@example.com";
+
+		PersonalInfo p5 = new PersonalInfo();
+		p5.name = "Rohit";
+		p5.age = 27;
+		p5.city = "Delhi";
+		p5.mobileNumber = 9872223334L;
+		p5.email = "rohit@example.com";
+
+		PersonalInfo p6 = new PersonalInfo();
+		p6.name = "Virat";
+		p6.age = 29;
+		p6.city = "Bangalore";
+		p6.mobileNumber = 9873334445L;
+		p6.email = "virat@example.com";
+
+		PersonalInfo p7 = new PersonalInfo();
+		p7.name = "Anshul";
+		p7.age = 26;
+		p7.city = "Hyderabad";
+		p7.mobileNumber = 9874445556L;
+		p7.email = "anshul@example.com";
+
+		PersonalInfo p8 = new PersonalInfo();
+		p8.name = "Mahesh";
+		p8.age = 30;
+		p8.city = "Surat";
+		p8.mobileNumber = 9875556667L;
+		p8.email = "mahesh@example.com";
+
+		PersonalInfo p9 = new PersonalInfo();
+		p9.name = "Priti";
+		p9.age = 21;
+		p9.city = "Kolhapur";
+		p9.mobileNumber = 9876667778L;
+		p9.email = "priti@example.com";
+
+		PersonalInfo p10 = new PersonalInfo();
+		p10.name = "Ovi";
+		p10.age = 22;
+		p10.city = "Goa";
+		p10.mobileNumber = 9877778889L;
+		p10.email = "ovi@example.com";
+
+		// Print All 10 Persons
+
+		System.out.println("\n------ Personal Info 1 ------");
+		System.out.println("Name: " + p1.name);
+		System.out.println("Age: " + p1.age);
+		System.out.println("City: " + p1.city);
+		System.out.println("Mobile No: " + p1.mobileNumber);
+		System.out.println("Email: " + p1.email);
+
+		System.out.println("\n------ Personal Info 2 ------");
+		System.out.println("Name: " + p2.name);
+		System.out.println("Age: " + p2.age);
+		System.out.println("City: " + p2.city);
+		System.out.println("Mobile No: " + p2.mobileNumber);
+		System.out.println("Email: " + p2.email);
+
+		System.out.println("\n------ Personal Info 3 ------");
+		System.out.println("Name: " + p3.name);
+		System.out.println("Age: " + p3.age);
+		System.out.println("City: " + p3.city);
+		System.out.println("Mobile No: " + p3.mobileNumber);
+		System.out.println("Email: " + p3.email);
+
+		System.out.println("\n------ Personal Info 4 ------");
+		System.out.println("Name: " + p4.name);
+		System.out.println("Age: " + p4.age);
+		System.out.println("City: " + p4.city);
+		System.out.println("Mobile No: " + p4.mobileNumber);
+		System.out.println("Email: " + p4.email);
+
+		System.out.println("\n------ Personal Info 5 ------");
+		System.out.println("Name: " + p5.name);
+		System.out.println("Age: " + p5.age);
+		System.out.println("City: " + p5.city);
+		System.out.println("Mobile No: " + p5.mobileNumber);
+		System.out.println("Email: " + p5.email);
+
+		System.out.println("\n------ Personal Info 6 ------");
+		System.out.println("Name: " + p6.name);
+		System.out.println("Age: " + p6.age);
+		System.out.println("City: " + p6.city);
+		System.out.println("Mobile No: " + p6.mobileNumber);
+		System.out.println("Email: " + p6.email);
+
+		System.out.println("\n------ Personal Info 7 ------");
+		System.out.println("Name: " + p7.name);
+		System.out.println("Age: " + p7.age);
+		System.out.println("City: " + p7.city);
+		System.out.println("Mobile No: " + p7.mobileNumber);
+		System.out.println("Email: " + p7.email);
+
+		System.out.println("\n------ Personal Info 8 ------");
+		System.out.println("Name: " + p8.name);
+		System.out.println("Age: " + p8.age);
+		System.out.println("City: " + p8.city);
+		System.out.println("Mobile No: " + p8.mobileNumber);
+		System.out.println("Email: " + p8.email);
+
+		System.out.println("\n------ Personal Info 9 ------");
+		System.out.println("Name: " + p9.name);
+		System.out.println("Age: " + p9.age);
+		System.out.println("City: " + p9.city);
+		System.out.println("Mobile No: " + p9.mobileNumber);
+		System.out.println("Email: " + p9.email);
+
+		System.out.println("\n------ Personal Info 10 ------");
+		System.out.println("Name: " + p10.name);
+		System.out.println("Age: " + p10.age);
+		System.out.println("City: " + p10.city);
+		System.out.println("Mobile No: " + p10.mobileNumber);
+		System.out.println("Email: " + p10.email);
+	}
+}
