1ebfcc8f62a918a2c390e396963a64cce5b7c690
Monika Jadhav <monikajadhav1211@gmail.com>
2025-11-26 19:11:52 +0530

    class with global variables

---
+++ OOP's Concept/Class and Object/Employee1.java
@@ -0,0 +1,12 @@
+package com.oops;
+
+public class Employee1 {
+
+// fields  // Variable // attributes 
+
+	public String empName;
+	public String city;
+	public int emp_Id;
+	public double salary;
+
+}
