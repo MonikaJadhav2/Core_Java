ad6baf6e0b5e36cffd9a36078fb7367ad92dd7c8
Monika Jadhav <monikajadhav1211@gmail.com>
2025-11-28 18:18:29 +0530

    Employee class method

---
+++ OOP's Concept/Methods/Employee.java
@@ -0,0 +1,8 @@
+package com.method;
+
+public class Employee {
+
+	void data(String fName, String lName) {
+		System.out.println(fName + " " + lName);
+	}
+}
---
+++ OOP's Concept/Methods/EmployeeMain.java
@@ -0,0 +1,11 @@
+package com.method;
+
+public class EmployeeMain {
+
+	public static void main(String[] args) {
+
+		Employee emp = new Employee();
+		emp.data("monika", "jadhav");
+	}
+
+}
