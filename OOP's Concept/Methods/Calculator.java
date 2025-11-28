bf6844cf93065784f6155eca813771107b23487c
Monika Jadhav <monikajadhav1211@gmail.com>
2025-11-28 18:08:01 +0530

    calculator operation using method

---
+++ OOP's Concept/Methods/Calculator.java
@@ -0,0 +1,22 @@
+package com.method;
+
+public class Calculator {
+
+	void add(int a, int b) {
+
+		System.out.println("Sum= " + (a + b));
+	}
+
+	void sub(int a, int b) {
+		System.out.println("Substraction = " + (a - b));
+	}
+
+	void mul(int a, int b) {
+		System.out.println("Multipplication = " + (a * b));
+	}
+
+	void div(int a, int b) {
+		System.out.println("Division = " + (a / b));
+	}
+
+}
---
+++ OOP's Concept/Methods/CalculatorInfo.java
@@ -0,0 +1,14 @@
+package com.method;
+
+public class CalculatorInfo {
+
+	public static void main(String[] args) {
+
+		Calculator c = new Calculator();
+		c.add(10, 3);
+		c.sub(10, 5);
+		c.mul(3, 4);
+		c.div(50, 5);
+	}
+
+}
