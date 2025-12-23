0c0347e9a761c0d7897746732c00f8afe092db50
Monika Jadhav <monikajadhav1211@gmail.com>
2025-12-23 18:18:34 +0530

    ex1

---
+++ OOP's Concept/FinalKeyword/Demo.java
@@ -0,0 +1,14 @@
+package com.finaltopic;
+
+public class Demo {
+
+	final long accNo =123456723;
+	public void m1() {
+		System.out.println("m1 method of Demo class");
+		 long accNo = 476543322;
+	}
+
+	public final  void pInfo() {
+		System.out.println("account no = 12334566778");
+	}
+}
---
+++ OOP's Concept/FinalKeyword/Main.java
@@ -0,0 +1,12 @@
+package com.finaltopic;
+
+public class Main extends Demo {
+
+	public static void main(String[] args) {
+
+		Demo d = new Demo();
+		d.pInfo();
+		d.m1();
+		
+	}
+}
