f9f137df72a550aea02a0ba768ca98ac267720fc
Monika Jadhav <monikajadhav1211@gmail.com>
2025-12-24 18:40:52 +0530

    access in different package without inheritance

---
+++ AccessSpecifiers/TestDifferentPackage.java
@@ -0,0 +1,23 @@
+package com.finaltopic;
+
+
+import accesss.AccessDemo;
+
+public class TestDifferentPackage {
+
+    public static void main(String[] args) {
+
+        AccessDemo obj = new AccessDemo();   // ✔ public constructor
+
+        System.out.println(obj.a);  // ✔ public
+        // System.out.println(obj.b); ❌ protected
+        // System.out.println(obj.c); ❌ default
+        // System.out.println(obj.d); ❌ private
+
+        obj.publicMethod();         // ✔
+        // obj.protectedMethod(); ❌
+        // obj.defaultMethod(); ❌
+        // obj.privateMethod(); ❌
+    }
+}
+
