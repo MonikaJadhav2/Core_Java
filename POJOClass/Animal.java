0604350ce2fc1b1901282d4235a737ca15c37fa0
Monika Jadhav <monikajadhav1211@gmail.com>
2025-12-25 19:07:34 +0530

    Example 6

---
+++ POJOClass/Animal.java
@@ -0,0 +1,8 @@
+package com.pojo;
+
+public class Animal {
+
+	void sound() {
+		System.out.println("Animal makes a sound");
+	}
+}
---
+++ POJOClass/Cat.java
@@ -0,0 +1,9 @@
+package com.pojo;
+
+public class Cat extends Animal {
+
+	@Override
+	void sound() {
+		System.out.println("Cat makes a sound");
+	}
+}
