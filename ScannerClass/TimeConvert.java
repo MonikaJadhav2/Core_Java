d5ccfd233eda2639897120c838061d4b5693776f
Monika Jadhav <monikajadhav1211@gmail.com>
2026-01-02 23:00:02 +0530

    seconds into min sec

---
+++ ScannerClass/TimeConvert.java
@@ -0,0 +1,19 @@
+package com.scanner1;
+import java.util.*;
+
+//Take time in seconds and convert into minutes and seconds.
+
+public class TimeConvert {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter seconds : ");
+		int s = sc.nextInt();
+		
+		System.out.println("Minutes: " + s/60);
+		System.out.println("Seconds : " + s%60);
+	}
+
+}
