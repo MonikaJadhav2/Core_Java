74e83b5a51c60679f0097570e14986afce17d15f
Monika Jadhav <monikajadhav1211@gmail.com>
2026-01-03 19:35:09 +0530

    Add files via upload

---
+++ OOP's Concept/Polymorphism/MethodOverloading/MainMethodExp.java
@@ -0,0 +1,20 @@
+package com.polymorphism.overloading;
+
+public class MainMethodExp {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	public static void main(float b) {
+
+	}
+
+	public static void  main(int a) {
+
+	}
+
+	public int main() {
+		return 123;
+	}
+}
