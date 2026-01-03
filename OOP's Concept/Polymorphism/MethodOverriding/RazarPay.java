95d95db658d077b8d5b8fa091b0bd475173fe526
Monika Jadhav <monikajadhav1211@gmail.com>
2026-01-03 14:36:07 +0530

    ex3

---
+++ OOP's Concept/Polymorphism/MethodOverriding/RazarPay.java
@@ -0,0 +1,13 @@
+package com.polymorphism.overriding;
+
+public class RazarPay extends Payment {
+
+	public void reward() {
+		System.out.println("Razarpay : Get the cashback of the 5 ruppes on friend Payments");
+	}
+
+	public void checkBalance() {
+		System.out.println("Razarpay : Check your account balance");
+	}
+
+}
