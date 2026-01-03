2151aab211d079e38c55c72752f3c824b1622295
Monika Jadhav <monikajadhav1211@gmail.com>
2026-01-03 19:34:20 +0530

    Add files via upload

---
+++ OOP's Concept/Polymorphism/MethodOverloading/Demo.java
@@ -0,0 +1,19 @@
+package com.polymorphism.overloading;
+
+public class Demo {
+	
+	public void show() {
+		System.out.println("This is overloading example");
+	}
+
+	public void show(int a ,String s) {
+   	 System.out.println("Value of a : " + a);
+   	 System.out.println("Value of String s : " + s);
+    }
+    
+    public static void main(String[] args) {
+		Demo d = new Demo();
+		d.show(78, "monika");
+    }
+    
+}
