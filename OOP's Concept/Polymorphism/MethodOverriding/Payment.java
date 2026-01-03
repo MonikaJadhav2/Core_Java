f51d28ac6c5dd024f86ef1a3423c83f71229e205
Monika Jadhav <monikajadhav1211@gmail.com>
2026-01-03 14:42:19 +0530

    ex3

---
+++ OOP's Concept/Polymorphism/MethodOverriding/Payment.java
@@ -0,0 +1,13 @@
+package com.polymorphism.overriding;
+
+public class Payment {
+
+	public void reward() {
+		System.out.println("Get cashback of the .....");
+	}
+
+	public void checkBalance() {
+		System.out.println("Check your account Balance.....");
+	}
+
+}
