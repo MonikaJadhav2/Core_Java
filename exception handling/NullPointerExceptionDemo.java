c92f1db31fd84c363410813a5f89ad6cc0b1556e
Monika Jadhav <monikajadhav1211@gmail.com>
2026-01-07 16:38:25 +0530

    Add files via upload

---
+++ exception handling/NullPointerExceptionDemo.java
@@ -0,0 +1,21 @@
+package exceptionHandling;
+
+public class NullPointerExceptionDemo {
+	
+              public static void main(String[] args) {
+            	  
+				String s =null;
+				
+				try {
+					
+					System.out.println("String length :" + s.length());
+					
+				}
+				
+				catch(NullPointerException n) {
+					
+					System.out.println("Accessing the null values");
+				}
+				
+			}
+}
