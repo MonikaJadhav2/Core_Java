f61c45dbc5b290e838bbea21ef78a582a590415d
Monika Jadhav <monikajadhav1211@gmail.com>
2026-01-07 17:21:29 +0530

    Add files via upload

---
+++ exception handling/ThrowsKeyword/A.java
@@ -0,0 +1,10 @@
+package com.throwsKeyword;
+
+public class A {
+
+
+	public void m1() throws ArithmeticException {
+		System.out.println(4 / 0);
+	}
+	
+}
---
+++ exception handling/ThrowsKeyword/MainClass.java
@@ -0,0 +1,13 @@
+package com.throwsKeyword;
+
+public class MainClass {
+
+
+	 public static void main(String args[]) throws ArithmeticException {
+	   
+	   B b = new B();
+	   b.m2();
+	  
+	   }
+	 
+}
