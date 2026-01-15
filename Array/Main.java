e150a3f60d646cb03937c609ce36a2c591530f9c
Monika Jadhav <monikajadhav1211@gmail.com>
2026-01-15 17:34:33 +0530

    Add files via upload

---
+++ Array/Main.java
@@ -0,0 +1,12 @@
+package com.tka;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int num [][] = {{10,20,50},{70,30,60}};
+		
+		System.out.println(num[2][1]);
+	}
+}
+        
