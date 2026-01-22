aabe0a49b8f83154d086d52c51c67736fe998620
Monika Jadhav <monikajadhav1211@gmail.com>
2026-01-22 16:10:53 +0530

    Add files via upload

---
+++ collectionFramework/Set/LinkedHashSet/LHSet.java
@@ -0,0 +1,27 @@
+package com.lset;
+
+import java.util.LinkedHashSet;
+
+public class LHSet {
+
+	public static void main(String[] args) {
+		
+//		1
+		
+		
+		
+		LinkedHashSet<Integer> set = new LinkedHashSet<Integer>();
+	
+		set.add(1234);
+		set.add(1);
+		set.add(null);
+		set.add(null);
+		set.add(4);
+		set.add(124);
+		set.add(134);
+		set.add(1234);   //maintains insertion order
+		
+		System.out.println(set);
+		
+	}
+}
