9d94149dad781777300cb0ed5f4ccd81e9ed6655
Monika Jadhav <monikajadhav1211@gmail.com>
2026-01-29 23:01:41 +0530

    Add CreateMap class to demonstrate HashMap usage

---
+++ collectionFramework/Map/CreateMap.java
@@ -0,0 +1,16 @@
+package com.map1;
+import java.util.*;
+
+public class CreateMap {
+
+	public static void main(String[] args) {
+
+        Map<Integer, String> map = new HashMap<>();
+
+        map.put(101, "Monika");
+        map.put(102, "Rahul");
+        map.put(103, "Sneha");
+
+        System.out.println("Map: " + map);
+    }
+}
