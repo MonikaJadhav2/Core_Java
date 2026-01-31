f875d5cd9600979222b8266d2ad8ec9ad4afadb3
Monika Jadhav <monikajadhav1211@gmail.com>
2026-01-31 23:17:38 +0530

    Add files via upload

--- Demo.java
+++ Demo.java
@@ -1,9 +1,21 @@
+package monika;
 
 public class Demo {
-
+	
 	public static void main(String[] args) {
 		
-		System.out.println("Hello world...!");
-		System.out.println("Welcome Monika");
+		String str = "Monika";
+		int count= 0;
+		
+		char [] ch = str.toCharArray();
+		
+		
+//		for each loop
+		for(char c: ch) {
+			count++;
+		}
+		
+		System.out.println("number of charcters: " + count);
 	}
+
 }
